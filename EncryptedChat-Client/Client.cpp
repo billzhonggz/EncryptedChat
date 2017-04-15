389cdad7334b37af7f49d24cf3b4d5e2c7cdc0b0
Bill ZHONG <billzhonggz@outlook.com>
2017-04-15 12:18:33 +0800

    Initial commit with a client which can send username of source and targets with the message and the original server from privious work.

---
+++ EncryptedChat-Client/Client.cpp
@@ -0,0 +1,179 @@
+/*
+	Encrypted Chatroom Client
+	Assignment 2
+	Internet & World Wide Web
+	1430003013 刘毓健 & 1430003045 钟钧儒
+*/
+
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define  DEFAULT_BUFFER 4096
+#define  DEFAULT_PORT	5019
+
+typedef struct
+{
+	SOCKET clientSock;
+	char *username;
+}sendThreadPara;
+
+DWORD WINAPI SendThread(LPVOID lpParam)
+{
+
+	sendThreadPara* sendPara = (sendThreadPara*)lpParam;
+	SOCKET sock = sendPara->clientSock;
+	char *username = sendPara->username;
+
+	char sendbuf[DEFAULT_BUFFER] = "";
+	char input[DEFAULT_BUFFER] = "";
+	int bytesSent, left, idx = 0;
+
+	//采取循环形式以确认信息完整发出，这是因为内核输出缓存有限制，输入信息有可能超过缓存大小
+	while (1)
+	{
+		// Push username of the sender at the front of the sending buffer.
+		strcat(sendbuf, "[");
+		strcat(sendbuf, username);
+		strcat(sendbuf, "]");
+		// Identify input format to the user.
+		printf("Input your message with the format \"[target1][target2][...]message\"\n");
+		fgets(input, DEFAULT_BUFFER, stdin);
+		// Combine sender's username at the front of the send information.
+		strcat(sendbuf, input);
+		left = strlen(sendbuf);
+		printf("Ready to send: %sLength: %d.\n", sendbuf, left);
+
+		// TODO: Encrypt before send. 
+
+		// Send message.
+		while (left > 0)
+		{
+			bytesSent = send(sock, &sendbuf[idx], left, 0);
+
+			if (bytesSent == 0)
+				return 1;
+			if (bytesSent == SOCKET_ERROR)
+			{
+				printf("send failed:%d", WSAGetLastError());
+				return 1;
+			}
+			left -= bytesSent;
+			idx += bytesSent;
+
+		}
+		idx = 0;
+		memset(sendbuf, 0, DEFAULT_BUFFER);
+	}
+	return 0;
+}
+
+DWORD WINAPI ReceiveThread(LPVOID lpParam)
+{
+	SOCKET sock = (SOCKET)lpParam;
+	int bytesRecv = SOCKET_ERROR;
+	char recvbuf[DEFAULT_BUFFER] = "";
+	while (1)
+	{
+		while (bytesRecv == SOCKET_ERROR) {
+			bytesRecv = recv(sock, recvbuf, DEFAULT_BUFFER, 0);
+
+			if (bytesRecv == 0 || bytesRecv == WSAECONNRESET) {
+				printf("Connection Closed.\n");
+				return 1;
+			}
+			if (bytesRecv < 0)
+				return 1;
+			printf("%s", recvbuf);
+		}
+		bytesRecv = SOCKET_ERROR;
+
+		memset(recvbuf, 0, DEFAULT_BUFFER);
+		return 0;
+	}
+}
+
+int main(void)
+{
+	// Define username by user.
+	printf("Please input your username, no more than 16 characters.\n");
+	char username[17] = { '\0' };
+	gets_s(username, 16);
+	printf("Your username is %s.\nBegin connection.\n", username);
+
+	// Initialize variables for threads.
+	HANDLE hThreadSend;
+	DWORD  sendThreadId;
+	HANDLE hThreadReceive;
+	DWORD  receiveThreadId;
+
+	// Initialize variables for winsock.
+	struct sockaddr_in server_addr;
+	struct hostent *hp;
+	SOCKET connect_sock;
+	WSADATA wsaData;
+	char			*server_name = "localhost";
+	unsigned short	port = DEFAULT_PORT;
+	unsigned int	addr;
+
+	// Try connect.
+	if (WSAStartup(0x202, &wsaData) == SOCKET_ERROR) {
+		// stderr: standard error are printed to the screen.
+		fprintf(stderr, "WSAStartup failed with error %d\n", WSAGetLastError());
+		//WSACleanup function terminates use of the Windows Sockets DLL. 
+		WSACleanup();
+		return -1;
+	}
+	if (isalpha(server_name[0]))
+		hp = gethostbyname(server_name);
+	else {
+		addr = inet_addr(server_name);
+		hp = gethostbyaddr((char*)&addr, 4, AF_INET);
+	}
+	if (hp == NULL)
+	{
+		fprintf(stderr, "Cannot resolve address: %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+	//copy the resolved information into the sockaddr_in structure
+	memset(&server_addr, 0, sizeof(server_addr));
+	memcpy(&(server_addr.sin_addr), hp->h_addr, hp->h_length);
+	server_addr.sin_family = hp->h_addrtype;
+	server_addr.sin_port = htons(port);
+
+
+	connect_sock = socket(AF_INET, SOCK_STREAM, 0);	//TCp socket
+
+	if (connect_sock == INVALID_SOCKET) {
+		fprintf(stderr, "socket() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	printf("Client connecting to: %s\n", hp->h_name);
+
+	if (connect(connect_sock, (struct sockaddr *)&server_addr, sizeof(server_addr))
+		== SOCKET_ERROR) {
+		fprintf(stderr, "connect() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+	
+	// Create two threads for send and recieve. 
+	// Assign parameters.
+	sendThreadPara sendPara;
+	sendPara.clientSock = connect_sock;
+	sendPara.username = username;
+	hThreadSend = CreateThread(NULL, 0, SendThread, &sendPara, 0, &sendThreadId);
+	hThreadReceive = CreateThread(NULL, 0, ReceiveThread, (LPVOID)connect_sock, 0, &receiveThreadId);
+
+	WaitForSingleObject(hThreadSend, INFINITE);
+	WaitForSingleObject(hThreadReceive, INFINITE);
+
+	closesocket(connect_sock);
+	WSACleanup();
+	system("pause");
+	return 0;
+}
---
+++ EncryptedChat-Server/server.cpp
@@ -0,0 +1,173 @@
+/*
+	Encrypted Chatroom Server
+	Assignment 2
+	Internet & World Wide Web
+	1430003013 刘毓健 & 1430003045 钟钧儒
+*/
+
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <fstream>
+#include<string>
+#include <time.h>
+using namespace std;
+
+#define DEFAULT_PORT	5019
+#define DEFAULT_BUFFER 4096
+
+//客户连接池，保存各个客户的连接
+vector<SOCKET> clients;
+
+typedef struct
+{
+	SOCKET sock;
+	sockaddr_in addr;
+	ofstream* out;
+} object;
+
+DWORD WINAPI clientThread(LPVOID lpParam)
+{
+	object* obj = (object*)lpParam;
+	SOCKET sock = obj->sock;
+	ofstream* fout = obj->out;
+
+	char buf[DEFAULT_BUFFER] = "";
+	char sendback[DEFAULT_BUFFER] = "";
+	int ret, left, idx = 0;
+
+	//循环处理客户端发送的信息
+	while (1)
+	{
+		ret = recv(sock, buf, DEFAULT_BUFFER, 0);
+		if (ret == 0)
+			return 1;
+		if (ret == SOCKET_ERROR)
+		{
+			printf("receive message failed:%d\n", WSAGetLastError());
+			return 1;
+		}
+
+		buf[ret] = '\0';
+
+		sprintf(sendback, "[%s said]:%s\n", inet_ntoa(obj->addr.sin_addr), buf);
+
+		printf("[The message from %s]:%s\n", inet_ntoa(obj->addr.sin_addr), buf);
+
+		printf("[The sendback message is]:  %s\n", sendback);
+
+		//向各个客户端发送通告信息
+		for (int i = 0; i < clients.size(); i++)
+		{
+
+			left = strlen(sendback);
+			while (left > 0)
+			{
+				ret = send(clients[i], &sendback[idx], left, 0);
+
+				if (ret == 0)
+					return 1;
+				else if (ret == SOCKET_ERROR)
+				{
+					printf("send back message failed:%d\n", WSAGetLastError());
+					return 1;
+				}
+
+				left -= ret;
+				idx += ret;
+
+			}
+			idx = 0;
+		}
+
+		memset(buf, 0, DEFAULT_BUFFER);
+		memset(sendback, 0, DEFAULT_BUFFER);
+	}
+
+	return 0;
+}
+
+
+
+int main(void) {
+
+	//char szBuff[100];
+	//int msg_len;
+	int addr_len;
+	struct sockaddr_in local, client_addr;
+	string record;
+
+	SOCKET listensock, client;
+	WSADATA wsaData;
+	HANDLE hThread;
+	DWORD  threadId;
+
+	if (WSAStartup(0x202, &wsaData) == SOCKET_ERROR) {
+		// stderr: standard error are printed to the screen.
+		fprintf(stderr, "WSAStartup failed with error %d\n", WSAGetLastError());
+		//WSACleanup function terminates use of the Windows Sockets DLL. 
+		WSACleanup();
+		return -1;
+	}
+	// Fill in the address structure
+	local.sin_family = AF_INET;
+	local.sin_addr.s_addr = INADDR_ANY;
+	local.sin_port = htons(DEFAULT_PORT);
+
+	listensock = socket(AF_INET, SOCK_STREAM, 0);	//TCp socket
+
+
+	if (listensock == INVALID_SOCKET) {
+		fprintf(stderr, "socket() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	if (bind(listensock, (struct sockaddr *)&local, sizeof(local)) == SOCKET_ERROR) {
+		fprintf(stderr, "bind() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	//waiting for the connections
+	if (listen(listensock, 5) == SOCKET_ERROR) {
+		fprintf(stderr, "listen() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	printf("Waiting for the connections ........\n");
+
+	while (1)
+	{
+		addr_len = sizeof(client_addr);
+		client = accept(listensock, (struct sockaddr*)&client_addr, &addr_len);
+		clients.push_back(client);
+		printf("Online client number: %d\n", clients.size());
+
+		if (client == INVALID_SOCKET)
+		{
+			fprintf(stderr, "accept() failed with error %d\n", WSAGetLastError());
+			break;
+		}
+
+		object obj;
+		obj.addr = client_addr;
+		obj.sock = client;
+		//为每个客户建立处理线程
+		hThread = CreateThread(NULL, 0, clientThread, (LPVOID)&obj, 0, &threadId);
+
+		if (hThread == NULL)
+		{
+			fprintf(stderr, "create thread failed with error %d\n", WSAGetLastError());
+			break;
+		}
+	}
+
+	closesocket(listensock);
+	WSACleanup();
+
+	return 0;
+}
