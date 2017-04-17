a4c418f83cc1d32b01ec22a6a80d7556edf2e89a
Mike <j430003013@mail.uic.edu.hk>
2017-04-17 13:20:43 +0800

    Prompts are shown at the beginning

--- EncryptedChat-Client/Client.cpp
+++ EncryptedChat-Client/Client.cpp
@@ -30,6 +30,9 @@ DWORD WINAPI SendThread(LPVOID lpParam)
 	char input[DEFAULT_BUFFER] = "";
 	int bytesSent, left, idx = 0;
 
+	// Identify input format to the user.
+	printf("Input your message with the format \"[target1][target2][...]message\"\nUse \"[server]command\" to access server.\nUse \"[server]userlist\" to see online users.\n");
+
 	//采取循环形式以确认信息完整发出，这是因为内核输出缓存有限制，输入信息有可能超过缓存大小
 	while (1)
 	{
@@ -37,8 +40,6 @@ DWORD WINAPI SendThread(LPVOID lpParam)
 		strcat(sendbuf, "[");
 		strcat(sendbuf, username);
 		strcat(sendbuf, "]");
-		// Identify input format to the user.
-		printf("Input your message with the format \"[target1][target2][...]message\"\nUse \"[server]command\" to access server.\nUse \"[server]userlist\" to see online users.\n");
 		fgets(input, DEFAULT_BUFFER, stdin);
 		// Combine sender's username at the front of the send information.
 		strcat(sendbuf, input);
